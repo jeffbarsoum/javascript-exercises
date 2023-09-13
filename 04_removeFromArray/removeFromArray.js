e2fce74702f7eed168f2d64ebd3ef29c66e3636f
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2023-09-13 00:58:46 -0400

    Solution for Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayToFilter = [], ...itemsToRemove) {
+	// We are sorting these arrays to make them easier to filter
+	// arrayToFilter.sort();
+	// itemsToRemove.sort();
+	const arrayToReturn = [];
 
+	// since the arrays are sorted, duplicates will always be next to each other
+	for (let i = 0; i < arrayToFilter.length; i++) {
+		let isRemove = false;
+		for (let j = 0; j < itemsToRemove.length; j++) {
+			if (arrayToFilter[i] === itemsToRemove[j]) {
+				isRemove = true;
+				break;
+			}
+		}
+		if (!isRemove) {
+			arrayToReturn.push(arrayToFilter[i]);
+		}
+	}
+	return arrayToReturn;
 };
 
 // Do not edit below this line
