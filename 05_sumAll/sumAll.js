abac5b97eebb0d6323e71471a00893bf5810edbb
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2023-09-13 18:42:30 -0400

    Solution for Exeercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (startInt, endInt) {
+	if (!(Number.isInteger(startInt) && Number.isInteger(endInt))) return "ERROR";
+	if (startInt < 0 || endInt < 0) return "ERROR";
+	let minNumber = startInt < endInt ? startInt : endInt;
+	const maxNumber = minNumber === startInt ? endInt : startInt;
 
+	let returnSum = 0;
+	for (minNumber; minNumber <= maxNumber; minNumber++) {
+		returnSum += minNumber;
+	}
+
+	return returnSum;
 };
 
 // Do not edit below this line
