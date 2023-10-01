a5e3c291e62b4e21c607fdd5ed0276b94d604a52
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2023-10-01 06:40:54 -0400

    Solution for Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function (seqNumber) {
+	if (seqNumber < 1) return "OOPS";
+	const fibSequence = [1, 1];
+	// console.log(
+	// fibSequence[fibSequence.length - 2],
+	// fibSequence[fibSequence.length - 1],
+	// );
+	for (let i = 2; i < seqNumber; i++) {
+		// console.log("i: ", i);
+		// console.table(fibSequence);
+		fibSequence.push(
+			fibSequence[fibSequence.length - 1] + fibSequence[fibSequence.length - 2],
+		);
+		// console.table(fibSequence);
+	}
+	// console.log("return value: ", fibSequence[fibSequence.length - 1]);
+	return fibSequence[fibSequence.length - 1];
 };
+
+// fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
