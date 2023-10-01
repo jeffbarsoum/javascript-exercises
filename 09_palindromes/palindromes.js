1d33042ecee9f2fb76899d1dab7a3a00f496c584
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2023-10-01 06:40:39 -0400

    Solution for Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+function removePunctuation(word) {
+	return word
+		.replace(/\s/g, "")
+		.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+		.replace(/\s{2,}/g, " ");
+}
 
+const palindromes = function (word) {
+	if (typeof word !== "string") return "ERROR";
+	const wordArray = removePunctuation(word).toLowerCase().split("");
+	const returnArray = [];
+
+	do {
+		returnArray.unshift(wordArray.shift());
+	} while (wordArray.length > 0);
+
+	return returnArray.join("") === removePunctuation(word).toLowerCase();
 };
-
+palindromes("A car, a man, a maraca.");
 // Do not edit below this line
 module.exports = palindromes;
